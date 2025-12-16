ebca0f5b0b58e70bf36fa509a41fb950698b8f0f
Dishank Devrani <dishank384@gmail.com>
2025-12-16 09:02:29 +0530

    Hello

--- All the college work/strings/question7.cpp
+++ All the college work/strings/question7.cpp
@@ -7,6 +7,7 @@ int main() {
     getline(cin, s1);
     getline(cin, s2);
 
+    if(s2.length()%2!=0){
     for (int i = 0; i < s1.length();) {
         if (i % 2 == 0) {
             s1.replace(i, 1, s2);
@@ -16,6 +17,18 @@ int main() {
             i++;
         }
     }
+}
+if(s2.length()%2==0){
+     for (int i = 0; i < s1.length();) {
+        if (i % 2 != 0||i==0) {
+            s1.replace(i, 1, s2);
+             i+=(s2.length()+1);
+        }
+        else{
+            i++;
+        }
+    }
+}
 
     cout << s1;
     return 0;
