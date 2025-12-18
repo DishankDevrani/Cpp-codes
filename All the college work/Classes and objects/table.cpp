012715e0319d574575f61cac815a5e61ed820993
Dishank Devrani <dishank384@gmail.com>
2025-12-18 17:42:18 +0530

    new updates

--- All the college work/Classes and objects/table.cpp
+++ All the college work/Classes and objects/table.cpp
@@ -6,7 +6,7 @@ class table{
 
     void table1(int n){
         for(int i=1;i<=10;i++){
-            cout<<n<<"*"<<i<<"="<<n*i<<endl;
+            cout<<n<<" x "<<i<<" = "<<n*i<<endl;
         }
     }
 };
